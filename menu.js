9d70c7c018a1c4fd353e098c5d3fee200bc5daa1
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-05-10 21:28:40 +0200

    Responsive card + menu + ajout logo home page +

--- menu.js
+++ menu.js
@@ -1,11 +1,9 @@
 let links = Array.from(document.querySelectorAll('.bar-link'))
-let icons = Array.from(document.querySelectorAll('.fa-solid'))
 
-var active;
-var iconActive;
+let active;
 
 
-for (var i = 0; i < links.length; i++) {
+for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', function (e) {
         if (active) {
             active.classList.remove('active')
